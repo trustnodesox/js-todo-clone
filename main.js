1fdb3c82569ba8d198158715cd0200954a1ff679
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-17 21:51:59 +0100

    con commenti, da rivedere il keyup

--- main.js
+++ main.js
@@ -2,21 +2,27 @@
 //Rendiamo il tutto gradevole alla vista.
 
 $(document).ready(function(){
+
+    //faccio sì che al click sul bottone venga aggiunto ciò che l'utente digita
   $('#aggiungi').click(function(){
     var inserisciQualcosa = $('#inserisciQualcosa').val();
     $('#InserisciQualcosa').val('');
+    //uso il clone in modo tale da aggiungere un li ogni volta che l'utente preme su aggiungi
     var template = $('#todo li').clone();
     template.children('span.testo').text(inserisciQualcosa);
+    //al click sull'icon checked l'li aggiunto padre di span viene eliminato
     template.children('span.fatto').click(function(){
       $(this).parent().remove();
     });
-    template.children('span.modifica').click(function(){
-      var titleDOM = $(this).siblings('span.testo');
-      var testo = titleDOM.text();
-      titleDOM.hide();
 
+    //al click sullo span .modifica permetto all'utente di modificare ciò che ha scritto in precedenza
+    template.children('span.modifica').click(function(){
+      var testoDOM = $(this).siblings('span.testo');
+      var testo = testoDOM.text();
+      testoDOM.hide();
       $(this).siblings('input').val(testo).show();
     });
+    //alla pressione del tasto invio il testo dell'utente viene inserito
     template.children('input').keyup(function(e){
       if (e.which == 13) {
         var testo = $(this).val();
@@ -24,6 +30,7 @@ $(document).ready(function(){
         $(this).siblings('span.testo').text(testo).show();
       }
     });
+    //concateno il template alla lista
     $('#lista').append(template)
   });
 });
